7f92b03cafda1bc1ae7f64cdec4ce96a990e041e
Torycd <vicidundun@gmail.com>
2024-01-23 10:34:48 +0100

    getting there

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,14 @@
-
+import croodImage from "../assets/croodscartoon.png";
 
 const HomePage = () => {
   return (
-    <div className="h-full col-span-8 bg-slate-50">HomePage</div>
-  )
-}
+    <div className="h-screen col-span-10 md:col-span-8 bg-slate-50">
+      <div className="border-2 border-[#2cb758] flex md:hidden justify-center p-1 rounded-full w-[50px] sm:w-[70px] m-4">
+        <img src={croodImage} className="rounded-full w-full" alt="#" />
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/components/SIdeBar.jsx
+++ src/components/SIdeBar.jsx
@@ -2,12 +2,12 @@ import croodImage from "../assets/croodscartoon.png";
 
 const SIdeBar = () => {
   return (
-    <div className="flex flex-col col-span-2 shadow-xl bg-white mr-4 w-full h-screen">
-      <div className="flex flex-col mx-auto mt-7">
-        <div className="border-2 border-[#2cb758] flex justify-center p-1 rounded-full w-[100px] mb-4">
+    <div className="hidden md:flex flex-col  md:col-span-2 shadow-xl bg-white mr-4 w-full h-screen">
+      <div className=" flex flex-col mx-auto mt-7">
+        <div className="border-2 border-[#2cb758] flex justify-center p-1 rounded-full w-[50px] sm:w-[70px] md:w-[100px] mb-4">
           <img src={croodImage} className="rounded-full w-full" alt="#" />
         </div>
-        <p className="font-medium ">Croods Family</p>
+        <p className="font-medium ">Croods_Family</p>
       </div>
     </div>
   );
