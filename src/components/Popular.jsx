81bf89a03dfe0ea1f148059daf7bf80d2a91182f
Torycd <vicidundun@gmail.com>
2024-01-24 12:25:04 +0100

    getting it done

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,27 +1,17 @@
-import coco from '../assets/coco.jpeg';
-import moana from "../assets/moana.jpeg"
-import crocs from '../assets/crocs.jpeg'
+import { DUMMY_POPULAR } from './store/cartoon_product';
 
 const Popular = () => {
   return (
     <div>
         <p className="text-bold mb-2 text-2xl">Popular</p>
         <div className="grid gap-3 text-white font-bold md:grid-cols-3 ">
-            <div className='p-3 rounded-md bg-gradient-to-tr from-[#ed523f] to-[#f7897b]  md:hover:translate-y-4 duration-100 ease-in-out'>
-                <img src={coco} className='rounded-md' alt="#"/>
-                <p className='mt-2'>Coco</p>
-                <p>2017 - Animation, Adventure</p>
+            {DUMMY_POPULAR.map((toon) => (
+                <div key={toon.id} className={toon.style}>
+                <img src={toon.image} className='rounded-md' alt="#"/>
+                <p className='mt-2'>{toon.title}</p>
+                <p>{toon.genre}</p>
             </div>
-            <div className='p-3 rounded-md bg-gradient-to-tr from-[#5698f3] to-[#9abcec]  md:hover:translate-y-4 duration-100 ease-in-out'>
-                <img src={moana} className='rounded-md' alt="#"/>
-                <p className='mt-2'>Moana</p>
-                <p>2021 - Animation, Adventure</p>
-            </div>
-            <div className='p-3 rounded-md bg-gradient-to-tr from-[#2cb758] to-[#7be39c]  md:hover:translate-y-4 duration-100 ease-in-out'>
-                <img src={crocs} className='rounded-md' alt="#"/>
-                <p className='mt-2'>Croc Boy</p>
-                <p>2023 - Animation, Adventure</p>
-            </div>
+            ))}
         </div>
     </div>
   )
---
+++ src/components/store/cartoon_product.js
@@ -0,0 +1,30 @@
+import coco from "../../assets/coco.jpeg";
+import moana from "../../assets/moana.jpeg";
+import crocs from "../../assets/crocs.jpeg";
+
+export const DUMMY_POPULAR = [
+  {
+    id: "p1",
+    image: coco,
+    style:
+      "p-3 rounded-md bg-gradient-to-tr from-[#ed523f] to-[#f7897b]  md:hover:translate-y-4 duration-100 ease-in-out",
+    title: "Coco",
+    genre: "2017 - Animation, Adventure",
+  },
+  {
+    id: "p2",
+    image: moana,
+    style:
+      "p-3 rounded-md bg-gradient-to-tr from-[#5698f3] to-[#9abcec]  md:hover:translate-y-4 duration-100 ease-in-out",
+    title: "Moana",
+    genre: "2021 - Animation, Adventur",
+  },
+  {
+    id: "p2",
+    image: crocs,
+    style:
+      "p-3 rounded-md bg-gradient-to-tr from-[#2cb758] to-[#7be39c]  md:hover:translate-y-4 duration-100 ease-in-out",
+    title: "Croc Boy",
+    genre: "2023 - Animation, Adventur",
+  },
+];
